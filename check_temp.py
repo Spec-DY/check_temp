bccea56630a56feacee16986239acb87689db21c
Spectrum <125960879+Spec-DY@users.noreply.github.com>
2024-03-07 00:05:52 -0800

    Update check_temp.py

--- check_temp.py
+++ check_temp.py
@@ -7,6 +7,7 @@ DEFAULT_CHECK = 3
 WARNING_COLOR = "\033[38;2;255;165;0m"
 RESET = "\033[0m"
 
+
 def get_cpu_temperature():
     res = os.popen("vcgencmd measure_temp").readline()
     return float(res.replace("temp=","").replace("'C\n",""))
@@ -19,10 +20,10 @@ def print_temp():
     if (temp >= HIGH_TEMP):
         print(f"{WARNING_COLOR}HIGH WARNING TEMP!{RESET}")
 
+
 def get_int(user_input = DEFAULT_CHECK):
     print("=======================================\n")
     while True:
-        
         try:
             user_input = input("How many times do you want to measure?\n")
             if (user_input == ""):
@@ -34,6 +35,7 @@ def get_int(user_input = DEFAULT_CHECK):
         except ValueError:
             print("Please enter an integer.\n")
 
+
 def main():
     print("-----------------------")
     print("|WELCOME TO TEMP CHECK|")
@@ -42,6 +44,7 @@ def main():
     for i in range(0,user_input):
         print_temp()
         time.sleep(WAIT_SECONDS)
+
 
 if __name__ == "__main__":
     main()
